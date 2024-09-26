7620868ccc3b19bb77794b4e1b9630227025abe4
Ghada-Mohamed-Khalaf <gmo625112@gmail.com>
2024-09-26 15:30:11 +0300

    create splash_view_body

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,10 +1,14 @@
-// import 'package:flutter/cupertino.dart';
-//
-// class SplashViewBody extends StatelessWidget {
-//   const SplashViewBody({super.key});
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Column();
-//   }
-// }
+import 'package:flutter/cupertino.dart';
+
+class SplashViewBody extends StatelessWidget {
+  const SplashViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [
+
+      ],
+    );
+  }
+}
