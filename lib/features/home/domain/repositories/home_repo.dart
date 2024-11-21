448b12bff7161933a857579500091bbc6f41723c
Ghada-Mohamed-Khalaf <gmo625112@gmail.com>
2024-11-21 14:46:52 +0200

    update either class

--- lib/features/home/domain/repositories/home_repo.dart
+++ lib/features/home/domain/repositories/home_repo.dart
@@ -1,7 +1,8 @@
+import 'package:bookly_app_clean/core/errors/failure.dart';
 import 'package:bookly_app_clean/features/home/domain/entity/book_entity.dart';
 import 'package:dartz/dartz.dart';
 
 abstract class HomeRepo{
-  Future<Either> fetchFeatureBooks();
-  Future<Either> fetchNewestBooks();
+  Future<Either<Failure,BookEntity>> fetchFeatureBooks();
+  Future<Either<Failure,BookEntity>> fetchNewestBooks();
 }
