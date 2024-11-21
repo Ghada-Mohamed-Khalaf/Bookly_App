2a7b9fada118d6a4e06e94e2dc91b073f6e5f046
Ghada-Mohamed-Khalaf <gmo625112@gmail.com>
2024-11-21 14:29:58 +0200

    add dartz package  and add either class

--- lib/features/home/domain/repositories/home_repo.dart
+++ lib/features/home/domain/repositories/home_repo.dart
@@ -1,6 +1,7 @@
 import 'package:bookly_app_clean/features/home/domain/entity/book_entity.dart';
+import 'package:dartz/dartz.dart';
 
 abstract class HomeRepo{
-  Future<List<BookEntity>> fetchFeatureBooks();
-  Future<List<BookEntity>> fetchNewestBooks();
+  Future<Either> fetchFeatureBooks();
+  Future<Either> fetchNewestBooks();
 }
